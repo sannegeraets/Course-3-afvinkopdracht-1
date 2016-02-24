4c97574f9603c1f6ca3a1d7f8d0a12c9436b91a4
Sanne Geraets <sannegeraets@hotmail.com>
2016-02-24 14:11:39 +0100

    Changes
    Verkeerd bestand verwijderd, goed bestand geupload

---
+++ Afvinkopdr 1.py
@@ -0,0 +1,50 @@
+def main():
+    bestand = openbestand()
+    seqq = sequentie(bestand)
+    seq = sfilter(seqq)
+    print(seq)
+    codons = create_codons(seq)
+    print(" ".join(codons))
+
+
+def openbestand():
+    try:
+        file = open("m_p53.gb", "r")
+        bestand = file.read().upper()
+        return bestand
+    except IOError:
+        print("Er is iets fout gegaan bij het openen van het bestand, probeer het opnieuw.")
+    except:
+        print("Er is iets fout gedaan.")
+
+
+def sequentie(bestand):
+    seq = bestand.split("ORIGIN")
+    return seq[1]
+
+
+def sfilter(seqq):
+    seq1 = []
+    for i in seqq:
+        if i == "A" or i == "C" or i == "G" or i == "T":
+            seq1.append(i)
+    seq = "".join(seq1)
+    return seq
+
+
+def create_codons(seq):
+    start = seq.find("ATG")
+    done = 0
+    codons = []
+    print(start)
+    if start != -1:
+        while start + 2 < len(seq) and done != 1:
+            codon = seq[start:start + 3]
+            if codon == "TAG" or codon == "TAA" or codon == "TGA":
+                done = 1
+            if not codon == "TAG" or codon == "TAA" or codon == "TGA":
+                codons.append(codon)
+                start += 3
+    return codons
+
+main()
